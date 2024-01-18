749e42f445551a54690f56bd0ae1a98c77b34271
snaeem20 <156692041+snaeem20@users.noreply.github.com>
2024-01-18 12:02:48 +0500

    C# Test 01

---
+++ CSharpTest01/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace CSharpTest01
+{
+    internal class Program
+    {
+
+        ///TASK #1: a) Create a class representing Laptop with the following fields: Manufacturer, OperationSystem, Memory, BatteryLife.
+        ///b) The class should also have a constructor taking setting the values of all the member fields.
+        ///c) Create a method ShowConfiguration that will display the properties.
+        ///d) Create an object of this class and call the method ShowConfiguration;
+
+        ///TASK #2: Create an overload method for the given method:
+
+        ///TASK #3: Create a method that takes a list of values as parameter and prints it 10 times - Note: Use both for and foreach loop
+
+        ///TASK #4: Create a generic method that will take two numeric arguments of types T1 and T2. This method will calculate the sum of
+        ///these values and prints the DOUBLE of this sum.
+
+
+        /// <summary>
+        /// Main Method
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test attempted by: --------------");
+
+            Console.WriteLine("TASK #1-d:");
+        }
+
+
+
+    }
+}
