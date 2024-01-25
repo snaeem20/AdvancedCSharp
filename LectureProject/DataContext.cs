388926997bd94080fd39c050a8cee099015894ac
snaeem20 <156692041+snaeem20@users.noreply.github.com>
2024-01-25 14:29:10 +0500

    sql connection

---
+++ LectureProject/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LectureProject
+{
+    internal class DataContext
+    {
+        string constr = "Database=Library;Server=WSAMZN-TB43SPPE\\mainline;Integrated Security=False;uid=TSGMeridian;Password=alp;";
+        SqlConnection _con = new SqlConnection(constr);
+        SqlDataAdapter _da = new SqlDataAdapter("Select * From Country", constr);
+        DataTable _dt = new DataTable();
+        _da.Fill(_dt);
+    }
+}
